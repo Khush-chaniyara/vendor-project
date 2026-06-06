a96f589a6cef38bf4cbbf3e0ee5fd7c3fe1c495c
khush chaniyara <khushkhush658@gmail.com>
2026-06-06 16:13:01 +0530

    Update dashboard.php

--- vendor/dashboard.php
+++ vendor/dashboard.php
@@ -1,14 +1,14 @@
 <?php 
 include 'header.php'; 
 
-// Fetch the vendor safely
+// Fetch the vendor 
 $vendor_email = $_SESSION['email']; 
 $v_query = mysqli_query($conn, "SELECT vendor_id FROM vendors WHERE email = '$vendor_email'");
 $vendor = mysqli_fetch_assoc($v_query);
 
 $v_id = $vendor ? $vendor['vendor_id'] : 0;
 
-// Get counts
+// Get counts 
 $rfq_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM rfq_vendor_assignments WHERE vendor_id = $v_id")) : 0;
 $quote_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM quotations WHERE vendor_id = $v_id")) : 0;
 $po_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM purchase_orders WHERE vendor_id = $v_id")) : 0;
@@ -20,7 +20,6 @@ $po_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM pur
 </div>
 
 <div class="row g-3">
-    <!-- RFQs Assigned Card -->
     <div class="col-md-4">
         <a href="open_rfqs.php" class="text-decoration-none">
             <div class="stat-card border-bottom border-primary border-3">
@@ -30,7 +29,6 @@ $po_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM pur
         </a>
     </div>
     
-    <!-- Quotes Submitted Card -->
     <div class="col-md-4">
         <a href="my_quotations.php" class="text-decoration-none">
             <div class="stat-card border-bottom border-warning border-3">
@@ -40,7 +38,6 @@ $po_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM pur
         </a>
     </div>
     
-    <!-- Active POs Card -->
     <div class="col-md-4">
         <a href="my_pos.php" class="text-decoration-none">
             <div class="stat-card border-bottom border-success border-3">
@@ -51,7 +48,6 @@ $po_count = ($v_id > 0) ? mysqli_num_rows(mysqli_query($conn, "SELECT * FROM pur
     </div>
 </div>
 
-<!-- Quick Actions -->
 <div class="row mt-4">
     <div class="col-12">
         <div class="card shadow-sm border-0 p-4">
