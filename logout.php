938c675480aadfd42ed9d10d7dd14ec21f28c289
khush chaniyara <khushkhush658@gmail.com>
2026-06-06 16:04:01 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,6 @@
 <?php
 session_destroy();
 
-// Start a fresh session specifically to pass a success message to the login page
 session_start();
 $_SESSION['success'] = "You have been successfully logged out.";
 
